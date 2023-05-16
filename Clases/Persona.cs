cbde085ef293f372f01d3a4a0c3408e1d0315a0f
ArthurFkOne <arturohrh@gmail.com>
2023-05-16 12:29:15 -0500

    practica uno

---
+++ Clases/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_de_mayo.Clases
+{
+    //clases es una plantilla 
+    //que contiene metodos y objetos
+
+     public class Persona
+    {
+
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public double Salario { get; set; }
+
+
+
+
+        public void ImprimirDatos()
+        {
+
+            Console.WriteLine("Tu nombre es : "+this.Nombre);
+            Console.WriteLine("Tu apellido es : " + this.Apellidos);
+            Console.WriteLine("Tu edad es : " + this.Edad);
+            Console.WriteLine("Tu salario es : " + this.Salario);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using _16_de_mayo.Clases;
+using System;
+
+namespace _16_de_mayo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Persona persona = new Persona();
+
+            persona.Nombre = "Raziel";
+            persona.Apellidos = "Martinez";
+            persona.Edad = 19;
+            persona.Salario= 20.00;
+            persona.ImprimirDatos();
+            Console.WriteLine(persona.Nombre);
+
+            persona.Nombre = "Carlos";
+            persona.Apellidos = "Jimenez";
+            persona.Edad = 19;
+            persona.Salario = 1400.00;
+            persona.ImprimirDatos();
+            Console.WriteLine(persona.Nombre);
+
+        }
+    }
+}
